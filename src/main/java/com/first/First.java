dd44f462b8edea21409d3fb303ad1506899b25fd
Shubham Kumar <Shubham.Kumar@Geminisolutions.com>
2022-09-27 10:45:26 +0530

    first blog v1

---
+++ src/main/java/com/first/First.java
@@ -0,0 +1,8 @@
+package com.first;
+
+public class First {
+
+    public static void main(String[] args) {
+        System.out.println("This is first class");
+    }
+}
---
+++ src/main/java/com/util/FirstUtil.java
@@ -0,0 +1,7 @@
+package com.util;
+
+public class FirstUtil {
+    public static void main(String[] args) {
+        System.out.println("First util class");
+    }
+}
